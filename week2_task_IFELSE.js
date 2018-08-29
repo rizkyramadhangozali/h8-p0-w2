a95474f9cfc38300b555ba2a53afed2468a8aad4
Rizky Ramadhan <rizkyramadhangozali@gmail.com>
2018-08-29 22:53:55 +0700

    upload ulang ifelse

--- week2_task_IFELSE.js
+++ week2_task_IFELSE.js
@@ -1,20 +1,24 @@
-var nama = 'kyo';
-var peran = 'Tabib';
+var nama = 'rizky'
+var peran = 'ksatria'
 
-if (nama === 'kyo' || peran) {
-  console.log('\"Selamat datang di dunia proxytia, kyoo\"') ;
-} else if (nama === 'kyo') {
-  console.log('\"Halo kyo, Pilih peranmu untuk memulai game]"') ;
-} else if (nama === '') {
-  console.log('\"Nama harus diisi!\"') ;
-} else {
-  console.log('\"Nama sudah dipakai\"') ;
+if ( nama === '' && peran === '') {
+  console.log('Nama harus diisi!')
 }
-if (peran === 'Ksatria') {
-  console.log('\"Halo Ksatria kyo, kamu dapat menyrang denga senjatamu!\"') ;
-} else if (peran === '') {
-  console.log('\"Peran harus diisi!\"') ;
-} else if (peran === 'Tabib') {
-  console.log('\"Halo Tabib kyo, kamu akan membantu temanmu yang terluka\"') ;
-} else if (peran === 'Penyihir') {
-  console.log('\"Halo Penyihir kyo, Ciptakan keajaiban yang membantu kemenanganmu!\"') ;
+else if( nama !== '' && peran === '' ) {
+  console.log('Halo ' + nama + ' pilih peranmu untuk memulai game!')
+}
+else if ( nama !== '' && peran === 'ksatria' ) {
+    console.log('Selamat datang di dunia Proxytia, ' + nama)
+    console.log( 'Halo ' + peran + ' ' + nama + ', ' + ' kamu dapat menyerang dengan senjatamu!')
+}
+else if ( nama !== '' && peran === 'tabib' ) {
+    console.log('Selamat datang di dunia Proxytia, ' + nama)
+    console.log( 'Halo ' + peran + ' ' + nama + ', ' + ' kamu akan membantu temanmu yang terluka!')
+}
+else if ( nama !== '' && peran === 'penyihir' ) {
+    console.log('Selamat datang di dunia Proxytia, ' + nama)
+    console.log( 'Halo ' + peran + ' ' + nama + ', ' + ' ciptakan keajaiban yang membantu kemenanganmu!')
+}
+else {
+  console.log ('Nama harus diisi!')
+}
